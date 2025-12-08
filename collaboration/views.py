f7be07e087a90a90ebaadeadc22a2a6654d879b6
James <qjaymce37@gmail.com>
2025-12-08 21:34:29 +0000

    modify collaboration/views.py

--- collaboration/views.py
+++ collaboration/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Collaboration
+from .serializers import CollaborationSerializer
 
-# Create your views here.
+class JoinProjectView(generics.CreateAPIView):
+    serializer_class = CollaborationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
