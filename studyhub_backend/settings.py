7f546dae2a0d0b8a99f29ace4b6304ca105ff670
James <qjaymce37@gmail.com>
2025-12-08 21:13:01 +0000

    add rest_framework.authtoken to settings.py

--- studyhub_backend/settings.py
+++ studyhub_backend/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
     'projects',
-    'collaboration'
+    'collaboration',
     'rest_framework',
     'rest_framework.authtoken',
 ]
