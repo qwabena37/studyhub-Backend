951e9402c28134a08d93e382ffefc4cca97ab54b
James <qjaymce37@gmail.com>
2025-12-08 21:24:10 +0000

    modify projects/models.py

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Project(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='projects')
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    tags = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.title
