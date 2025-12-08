df8abdd756911167c042f7c2af402a6181051560
James <qjaymce37@gmail.com>
2025-12-08 21:30:48 +0000

    modify collaboration/models.py

--- collaboration/models.py
+++ collaboration/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from projects.models import Project
 
-# Create your models here.
+class Collaboration(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    joined_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'project')
