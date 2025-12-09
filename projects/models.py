f63f492df19a2dc8c80c7cc3fd99d9af10c52b91
James <qjaymce37@gmail.com>
2025-12-09 17:20:38 +0000

    modify projects/models.py

--- projects/models.py
+++ projects/models.py
@@ -1,9 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 
 class Project(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
-    description = models.TextField(blank=True)
+    description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
