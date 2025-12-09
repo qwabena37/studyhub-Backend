24132d45a327539100ea8a33519b38c5aebf9065
James <qjaymce37@gmail.com>
2025-12-09 17:59:37 +0000

    modify middleware in settings.py

--- studyhub_backend/settings.py
+++ studyhub_backend/settings.py
@@ -27,6 +27,17 @@ INSTALLED_APPS = [
     "comments",
 ]
 
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
