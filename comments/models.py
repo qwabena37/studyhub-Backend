c558e86749cafe3fc89962ee10a0ba89fbeebd5a
James <qjaymce37@gmail.com>
2025-12-09 17:24:19 +0000

    modify comments/models.py

--- comments/models.py
+++ comments/models.py
@@ -1,11 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 from projects.models import Project
 
-class Collaboration(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    joined_at = models.DateTimeField(auto_now_add=True)
+class Comment(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="comments")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
-    class Meta:
-        unique_together = ('user', 'project')
+    def __str__(self):
+        return f"Comment by {self.user.username}"
