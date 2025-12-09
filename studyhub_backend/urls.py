06cbc61fc4fb90785d4abd5cb80355c8aa68caba
James <qjaymce37@gmail.com>
2025-12-09 21:45:05 +0000

    add custom HttpResponse

--- studyhub_backend/urls.py
+++ studyhub_backend/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.http import HttpResponse
+
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -19,6 +21,7 @@ schema_view = get_schema_view(
 
 
 urlpatterns = [
+    path("", lambda request: HttpResponse("Welcome to Studyhub API!")),
     path("admin/", admin.site.urls),
     path("api/accounts/", include("accounts.urls")),
     path("api/projects/", include("projects.urls")),
