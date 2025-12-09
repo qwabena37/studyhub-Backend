308f46b52d19c535ed59bed644d5f9e0a86a48cc
James <qjaymce37@gmail.com>
2025-12-09 17:15:56 +0000

    modify accounts/models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    # Extended profile fields
+    school = models.CharField(max_length=255, blank=True)
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.username
