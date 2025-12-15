6c523b6db73e9fc87fbc6b0ff87beaa54b6c1ea4
James <qjaymce37@gmail.com>
2025-12-15 18:05:56 +0000

    modify comments/urls.py

--- comments/urls.py
+++ comments/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import CommentListCreateView
 
 urlpatterns = [
+    path("", CommentListCreateView.as_view(), name="comment-list-all"),
     path("<int:project_id>/", CommentListCreateView.as_view()),
 ]
